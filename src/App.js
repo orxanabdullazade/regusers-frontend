ec35dcc7ba1b00f919fb93b28d2a043073259ace
orxanabdullazade <orxanzadeh@gmail.com>
2022-03-24 00:13:50 +0400

    test
    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Button } from 'antd';
 function App() {
   return (
     <div className="App">
-           <Button type="primary">Button</Button>
+           <Button type="primary">Button 1</Button>
     </div>
   );
 }
