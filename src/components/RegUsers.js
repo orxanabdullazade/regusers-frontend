51bcef64bba188b9bd46d61365152152f3683324
orxanabdullazade <orxanzadeh@gmail.com>
2022-04-03 01:20:54 +0400

    tag,icons,modal

--- src/components/RegUsers.js
+++ src/components/RegUsers.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import reactDom from "react-dom";
 import axios from "axios";
-import { Table, Tag, Space, Button } from "antd";
+import { Table, Tag, Modal, Form, Switch, Input } from "antd";
+import { FormOutlined } from "@ant-design/icons";
 
 const RegUsers = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const [dataSource, setDataSource] = useState([]);
   const [totalElements, setTotalElements] = useState(1);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingRegUser, setEditingRegUser] = useState(null);
   // const [page, setPage] = useState(1);
   // const [pageSize, setPageSize] = useState(10);
 
@@ -23,27 +26,39 @@ const RegUsers = () => {
     {
       title: "Email təsdiq",
       dataIndex: "status",
-      render: status => (
-        <Tag color={status==9?"green":"red"}>{status==9?"Təsdiq edilib":"Təsdiq gözləyir"}</Tag>
+      render: (status) => (
+        <Tag color={status == 9 ? "green" : "red"}>
+          {status == 9 ? "Təsdiq edilib" : "Təsdiq gözləyir"}
+        </Tag>
       ),
     },
     {
       title: "Telefon təsdiq",
       dataIndex: "mobileStatus",
-      render: status => (
-        <Tag color={status==9?"green":"red"}>{status==9?"Təsdiq edilib":"Təsdiq gözləyir"}</Tag>
+      render: (status) => (
+        <Tag color={status == 9 ? "green" : "red"}>
+          {status == 9 ? "Təsdiq edilib" : "Təsdiq gözləyir"}
+        </Tag>
       ),
     },
     {
       title: "Action",
       dataIndex: "re",
-      render: () => (
-        <Space size="middle">
-          <a>Delete</a>
-        </Space>
+      render: (record) => (
+        <FormOutlined
+          onClick={() => {
+            editRegUser(record);
+          }}
+        />
       ),
-    }
+    },
   ];
+
+  const editRegUser = (record) => {
+    setIsEditing(true);
+    console.log({...record});
+    setEditingRegUser({ ...record });
+  };
 
   const fetchRecords = (page) => {
     setLoading(true);
@@ -65,19 +80,34 @@ const RegUsers = () => {
     return <div>Error !!!</div>;
   } else {
     return (
-      <Table
-        rowKey={record => record.loginName}
-        loading={loading}
-        columns={columns}
-        dataSource={dataSource}
-        pagination={{
-          pageSize: 8,
-          total:totalElements,
-          onChange: (page) => {
-            fetchRecords(page-1);
-          },
-        }}
-      />
+      <div>
+        <Table
+          rowKey={(record) => record.loginName}
+          loading={loading}
+          columns={columns}
+          dataSource={dataSource}
+          pagination={{
+            pageSize: 8,
+            total: totalElements,
+            onChange: (page) => {
+              fetchRecords(page - 1);
+            },
+          }}
+        />
+        <Modal
+          onCancel={() => {
+            setIsEditing(false);
+          }}
+          cancelText="Imtina"
+          visible={isEditing}
+          onOk={() => {
+            setIsEditing(false);
+          }}
+          okText="Yadda saxla"
+        >
+          <Input value={editRegUser?.loginName} />
+        </Modal>
+      </div>
     );
   }
 };
